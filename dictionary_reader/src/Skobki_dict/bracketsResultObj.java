22fc4c24bdf83325eb716f47d9a09c31cefada08
aleksoider <aleksoider@mail.ru>
2015-06-03 16:50:06 +0300

    added interfaces

---
+++ dictionary_reader/src/Skobki_dict/bracketsResultObj.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Skobki_dict;
+
+import dictionary_reader.resultObj;
+
+/**
+ *
+ * @author 123
+ */
+public class bracketsResultObj implements resultObj{
+    
+}
